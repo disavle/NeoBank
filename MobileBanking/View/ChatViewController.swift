d47ff3e5944271f91945571975ae6a9986169022
Dima DIsavle <69400943+disavle@users.noreply.github.com>
2021-08-12 22:26:06 +0300

    TabBar is ready

---
+++ MobileBanking/View/ChatViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ActivityViewController.swift
+//  MobileBanking
+//
+//  Created by Dima Savelyev on 12.08.2021.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+
+        view.backgroundColor = .systemBackground
+        label = {
+            let label = UILabel()
+            label.text = "Chat"
+            label.textAlignment = .center
+            label.font = UIFont(name: "Kepler-296", size: 35)
+            label.textColor = .label
+            
+            view.addSubview(label)
+            label.snp.makeConstraints { maker in
+                maker.centerX.equalToSuperview()
+                maker.centerY.equalToSuperview()
+                maker.width.equalToSuperview().dividedBy(2)
+                maker.height.equalTo(50)
+            }
+            return label
+        }()
+    }
+}
